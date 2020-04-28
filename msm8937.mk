2a615e8b32f2896b02dc81985d9ecccc7414d401
yaelahsem <kagura.aileduo@gmail.com>
2020-04-28 17:01:02 +0000

    msm8937-common: move to common livedisplay

--- msm8937.mk
+++ msm8937.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl
 
+# Display calibration
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Doze mode
 PRODUCT_PACKAGES += \
     XiaomiDoze
