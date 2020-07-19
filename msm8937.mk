d11bddee38b65bb6331ca16b5ed994036460ab82
yaelahsem <kagura.aileduo@gmail.com>
2020-07-19 11:13:44 +0000

    msm8937-common: remove fuckin doze
    Signed-off-by: yaelahsem <kagura.aileduo@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -179,10 +179,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
